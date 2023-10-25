9437c42cfc2b40f13826297a65bb15d112f4c4c6
Valerie <valeria_serebrennikova@mail.ru>
2023-10-25 20:17:27 +0300

    Creating HTTP API

---
+++ src/main/kotlin/com/example/modules/User.kt
@@ -0,0 +1,15 @@
+package com.example.modules
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class User(
+    val id: String,
+    val name: String,
+    val email: String,
+    val password: String
+)
+
+val users = mutableListOf<User>()
+
---
+++ src/main/kotlin/com/example/plugins/Serialization.kt
@@ -0,0 +1,76 @@
+package com.example.plugins
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.example.modules.User
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import java.util.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+
+
+    val secret = environment.config.property("jwt.secret").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    val myRealm = environment.config.property("jwt.realm").getString()
+
+
+    install(Authentication) {
+        jwt("auth-jwt") {
+            realm = myRealm
+            verifier(
+                JWT
+                .require(Algorithm.HMAC256(secret))
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .build())
+            validate { credential ->
+                if (credential.payload.getClaim("name").asString() != "") {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+            challenge { defaultScheme, realm ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
+            }
+        }
+    }
+
+    routing {
+        post("/login") {
+            val user = call.receive<User>()
+            // Check username and password
+            // ...
+            val token = JWT.create()
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .withClaim("name", user.name)
+                .withExpiresAt(Date(System.currentTimeMillis() + 60000))
+                .sign(Algorithm.HMAC256(secret))
+            call.respond(hashMapOf("token" to token))
+        }
+        authenticate("auth-jwt") {
+            get("/hello") {
+                val principal = call.principal<JWTPrincipal>()
+                val username = principal!!.payload.getClaim("name").asString()
+                val expiresAt = principal.expiresAt?.time?.minus(System.currentTimeMillis())
+                call.respondText("Hello, $username! Token is expired at $expiresAt ms.")
+            }
+
+        }
+
+    }
+
+}
